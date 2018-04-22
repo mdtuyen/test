21380b06a9ac03f5924d80fa95174e25eabd5c14
funny.mobiledev <funny.mobiledev@gmail.com>
2018-04-22 21:16:52 +0700

    init

---
+++ app/src/main/assets/com.pvmobile.xemtivi/checkv4.php
@@ -0,0 +1,18 @@
+{
+  "link_country": "http://www.geoplugin.net/json.gp",
+  "value_country": "geoplugin_countryName",
+  "link_country2": "http://freegeoip.net/json/",
+  "value_country2": "country_name",
+  "link_check_country": "http://socks5list.net/LiveSoccerAppV5/lich-phat-song.php",
+  "link_popular": "https://raw.githubusercontent.com/mdtuyen/test/dev/app/src/main/assets/com.pvmobile.xemtivi/tv.php",
+  "link_vtv": "http://socks5list.net/LiveSoccerAppV5/tv_vtv.php",
+  "link_htv": "http://socks5list.net/LiveSoccerAppV5/tv_htv.php",
+  "link_vtc": "http://socks5list.net/LiveSoccerAppV5/tv_vtc.php",
+  "link_film": "http://socks5list.net/LiveSoccerAppV5/tv_film.php",
+  "link_sport": "http://socks5list.net/LiveSoccerAppV5/tv_sport.php",
+  "link_area": "http://socks5list.net/LiveSoccerAppV5/tv_area.php",
+  "link_other": "http://socks5list.net/LiveSoccerAppV5/tv_other.php",
+  "acceptcountry": [
+    "Vietnam"
+  ]
+}
