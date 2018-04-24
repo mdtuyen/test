f263d5bf217ba361d5cccc54babc67d6668d20d0
funny.mobiledev <funny.mobiledev@gmail.com>
2018-04-24 22:27:12 +0700

    init

--- app/src/main/assets/com.pvmobile.xemtivi/checkv3.php
+++ app/src/main/assets/com.pvmobile.xemtivi/checkv3.php
@@ -5,7 +5,7 @@
   "value_country2": "country_name",
   "link_check_country": "http://socks5list.net/LiveSoccerAppV5/lich-phat-song.php",
   "link_popular": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv.php",
-  "link_vtv": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_vtv.php",
+  "link_vtv": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_vtv_real.php",
   "link_htv": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_htv.php",
   "link_vtc": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_vtc.php",
   "link_film": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_film.php",
