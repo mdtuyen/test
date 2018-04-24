3301c5204a77bc234011247d2b5845d879a6d7f3
funny.mobiledev <funny.mobiledev@gmail.com>
2018-04-24 20:51:45 +0700

    init

--- app/src/main/assets/com.pvmobile.xemtivi/checkv4.php
+++ app/src/main/assets/com.pvmobile.xemtivi/checkv4.php
@@ -5,13 +5,13 @@
   "value_country2": "country_name",
   "link_check_country": "http://socks5list.net/LiveSoccerAppV5/lich-phat-song.php",
   "link_popular": "https://raw.githubusercontent.com/mdtuyen/test/dev/app/src/main/assets/com.pvmobile.xemtivi/tv.php",
-  "link_vtv": "http://socks5list.net/LiveSoccerAppV5/tv_vtv.php",
-  "link_htv": "http://socks5list.net/LiveSoccerAppV5/tv_htv.php",
-  "link_vtc": "http://socks5list.net/LiveSoccerAppV5/tv_vtc.php",
-  "link_film": "http://socks5list.net/LiveSoccerAppV5/tv_film.php",
-  "link_sport": "http://socks5list.net/LiveSoccerAppV5/tv_sport.php",
-  "link_area": "http://socks5list.net/LiveSoccerAppV5/tv_area.php",
-  "link_other": "http://socks5list.net/LiveSoccerAppV5/tv_other.php",
+  "link_vtv": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_vtv.php",
+  "link_htv": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_htv.php",
+  "link_vtc": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_vtc.php",
+  "link_film": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_film.php",
+  "link_sport": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_sport.php",
+  "link_area": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_area.php",
+  "link_other": "https://gitlab.com/dothitinhvnn/xwc18/raw/master/tv_other.php",
   "acceptcountry": [
     "Vietnam"
   ]
