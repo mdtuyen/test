df1da5f116d9b007d125957b272ee4d73d431b66
funny.mobiledev <funny.mobiledev@gmail.com>
2018-05-25 07:15:29 +0700

    init

---
+++ app/src/main/assets/com.livescore.xemtivi/tv_film.php
@@ -0,0 +1,162 @@
+{
+  "streaming": [
+    {
+      "status": "yes"
+    }
+  ],
+  "channels": [
+    {
+      "Name": "Max HBO",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/maxbyhbo.fpt.720p.stream/playlist.m3u8?st=7dhJqy_Z0iYcdOG5u-ZmTw&e=1523923206",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2017/08/09/571916-max-by-HBO.png"
+    },
+    {
+      "Name": "Red HBO",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/redhbo.fpt.720p.stream/playlist.m3u8?st=ZzNmRsNUtVlEbm7ZSeDElQ&e=1523923206",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "https://lh3.googleusercontent.com/-a-sgqaH-lQQ/WYqyP97--UI/AAAAAAAADkw/j7bYpfDsGrQ6mcOZpwMTZLX9CP4ad_wwwCLcBGAs/h120/1258630-unnamed.png"
+    },
+    {
+      "Name": "Star Movies",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/starmovies.FPT.720p.stream/playlist.m3u8?st=_P_guxH0JQ2yrrabgn-wUQ&e=1523924463",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/01/29/433546-Star_Movies_HD_logo_new.png"
+    },
+    {
+      "Name": "Cinemax",
+      "url": "http://mytv.tvmienphi.biz/mytv.php?id=cinemax&token=pt1_Ak89hBSsN9GrEnW9Vg&time=1523894405",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "https://lh3.googleusercontent.com/-1kxexJ2Tsj0/VpxAXRZa2lI/AAAAAAAAAKQ/uZuHN8CLWu0/h120/cinemax(1).png"
+    },
+    {
+      "Name": "STAR World",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/starworld.FPT.720p.stream/playlist.m3u8?st=bL1lbXjXNpGQ3qMbSLyEYg&e=1523924463",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/01/29/341514-Star_World_HD.png"
+    },
+    {
+      "Name": "Phim Hay",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/phimhay.FPT.480p.stream/playlist.m3u8?st=VjSkLmdvaeDrV6Za2YKihw&e=1523924223",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2016/07/15/901788-phim.hay_.png"
+    },
+    {
+      "Name": "HBO",
+      "url": "https://talk.vcdn.vn/hls/5ecb385e47813c0f4521a0880ef3f3e6/162cf418ca5/thegioionline/index.m3u8",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "https://lh3.googleusercontent.com/-9Q1IA887yas/VpxAefCF7cI/AAAAAAAAAMw/-nPsuo8Bob4/h120/hbo(1).png"
+    },
+    {
+      "Name": "Hoạt Hình",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/toonami.fpt.720p.stream/playlist.m3u8?st=Tq6lo2WwLjvyOXWq5XFPhg&e=1523924105",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2017/08/09/602255-tonami.png"
+    },
+    {
+      "Name": "Hoạt Hình 2",
+      "url": "http://mytv.tvmienphi.biz/mytv.php?id=disneyjunior&token=LgBm2BkHZc-DqOvzR14jmA&time=1523894406",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "https://i.imgur.com/Yt3nnmc.png"
+    },
+    {
+      "Name": "Disney",
+      "url": "https://talk2.vcdn.vn/hls/a53b118515e903b6de600c6adfb452b0/162cf4190f8/1102578626/index.m3u8",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "https://lh3.googleusercontent.com/-pB4lo73t5BU/VpxAaKT-9gI/AAAAAAAAALI/nqhTy7Xv2LE/h120/disney(1).png"
+    },
+    {
+      "Name": "HTVC Phim",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/htvcphim.480p.stream/playlist.m3u8?st=1BH6tvwjDmjtFxxcCN5FHA&e=1523923210",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2017/04/20/706707-Phim.png"
+    },
+    {
+      "Name": "Cartoon Netword",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/cartoonnetwork.fpt.720p.stream/playlist.m3u8?st=d5NumMVNu9KPjn6KqRPUJw&e=1523923205",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2017/08/09/120786-Cartoon-Netword-HD.png"
+    },
+    {
+      "Name": "Warner TV",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/wanertv.fpt.720p.stream/playlist.m3u8?st=7ogOAe10LfOoo-QbYKaAZw&e=1523924104",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2017/08/09/712174-WARNER-tv.png"
+    },
+    {
+      "Name": "Cinema World",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/cinemaworld.fpt.720p.stream/playlist.m3u8?st=Ksyec3BISzFiXEC8wuTEcQ&e=1523924105",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2017/08/09/503539-cinemaworld.png"
+    },
+    {
+      "Name": "SAM",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/sam.fpt.720p.stream/playlist.m3u8?st=Apjzc74iq9KmklwinC_Fag&e=1523924222",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2017/08/09/330368-sam.png"
+    },
+    {
+      "Name": "HTVC Phim HD",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/htvcphim.720p.stream/playlist.m3u8?st=q5wphKonq_ErVSog-hifVA&e=1523923211",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2017/04/20/592006-Phim_HD.png"
+    },
+    {
+      "Name": "iMovie HD",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/imovie.fpt.720p.stream/playlist.m3u8?st=edNOgvbEo2jvnefPnySjag&e=1523924223",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2017/08/09/958404-imovie-btv4.png"
+    }
+  ]
+}
