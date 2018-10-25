6cb681c92b4cb66198bf539db942d1315781a757
funny.mobiledev <funny.mobiledev@gmail.com>
2018-10-25 22:19:42 +0700

    init

---
+++ app/src/main/assets/com.bestsoft.lvbongda/tv_area.php
@@ -0,0 +1,351 @@
+{
+  "streaming": [
+    {
+      "status": "yes"
+    }
+  ],
+  "channels": [
+    {
+      "Name": "THVL2",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/vinhlong2.480p.stream/playlist.m3u8?st=2PKmcBR5oVTvj9ex-o0tAw&e=1523924402",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/460x260/poster/2016/08/24/482704-thvl.png"
+    },
+    {
+      "Name": "THVL1",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/vinhlong1.480p.stream/playlist.m3u8?st=iN1xbe7qNFVHVFIn8ZX0_Q&e=1523924402",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/460x260/poster/2016/08/24/398779-thvl1.png"
+    },
+    {
+      "Name": "Bình Dương 1",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/btv1.480p.stream/playlist.m3u8?st=eN1MF6_PtioX4heCcAbhaw&e=1523924402",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2015/10/08/976681-BTV-1.png"
+    },
+    {
+      "Name": "Bình Dương 2",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/btv2.480p.stream/playlist.m3u8?st=HUhnoQeSoA_KWOl32OrB6A&e=1523924403",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2015/10/08/904435-BTV-2.png"
+    },
+    {
+      "Name": "Tây Ninh",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/tayninh.480p.stream/playlist.m3u8?st=jOdH-YHmAMTEld-yw6j0wA&e=1523924403",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/25/199734-tay-ninh.png"
+    },
+    {
+      "Name": "Bến Tre",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/bentre.480p.stream/playlist.m3u8?st=l85NqWYULFEbCB2oIy1Ulg&e=1523924462",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2016/12/19/964668-ben_tre.png"
+    },
+    {
+      "Name": "Lâm Đồng",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/lamdong.480p.stream/playlist.m3u8?st=_FtxKirRfE8yKfA9XGEbBQ&e=1523924462",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2016/10/13/980683-lamdong.png"
+    },
+    {
+      "Name": "Bình Thuận",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/binhthuan.480p.stream/playlist.m3u8?st=g2g8s40FdtUHZXORBAIuMw&e=1523924462",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2016/09/15/801810-binh.thuan_.png"
+    },
+    {
+      "Name": "Đà Nẵng 2",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/danang2.480p.stream/playlist.m3u8?st=stm3Ve7h34Gptz2tPYc-SA&e=1523924462",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2016/09/15/419875-Da.Nang_.2_.png"
+    },
+    {
+      "Name": "An Giang",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/angiang.480p.stream/playlist.m3u8?st=wF0jd87Q76WW7k5WNap0yw&e=1523924463",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2016/08/10/866724-angiang.png"
+    },
+    {
+      "Name": "Long An",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/longan.480p.stream/playlist.m3u8?st=ZqlyzIF6e2Dywl9J-Inepw&e=1523924462",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/11/293780-longan-34.png"
+    },
+    {
+      "Name": "Đồng Tháp",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/dongthap.480p.stream/playlist.m3u8?st=y7v3qfO_UMiVmvS3QFs0mg&e=1523924462",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/24/336574-dong-thap.png"
+    },
+    {
+      "Name": "Kiên Giang",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/kiengiang.480p.stream/playlist.m3u8?st=hziOEYyEutSZ5WoUb1eGqA&e=1523924463",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/10/872209-Kiengiang_logo.png"
+    },
+    {
+      "Name": "Cần Thơ",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/cantho.480p.stream/playlist.m3u8?st=WeTjb9AKpnJrdUWdHYfGyw&e=1523924463",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/25/484569-can-tho.png"
+    },
+    {
+      "Name": "Hà Tĩnh",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/hatinh.480p.stream/playlist.m3u8?st=ysq2uRTqHVDaUftiNAKm6A&e=1523924463",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2015/05/20/492375-ha-tinh.png"
+    },
+    {
+      "Name": "Huế",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/hue.480p.stream/playlist.m3u8?st=pCz9R64M6--X6YV48e6szA&e=1523924463",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2016/05/19/422173-hue.png"
+    },
+    {
+      "Name": "Bình Phước",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/binhphuoc1.480p.stream/playlist.m3u8?st=KYrXdEZxk0rF3KWHqVL4xA&e=1523924463",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/11/450160-binh-phuoc-1.png"
+    },
+    {
+      "Name": "Trà Vinh",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/travinh.480p.stream/playlist.m3u8?st=WtlCg0TCR8rPc_Wfg-QvYA&e=1523923210",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/03/06/445259-tra-vinh.png"
+    },
+    {
+      "Name": "Đà Nẵng 1",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/danang1.480p.stream/playlist.m3u8?st=LRMiErgqJatVgYp1mwD3Mw&e=1523923211",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2016/08/03/216878-Da_Nang_1.png"
+    },
+    {
+      "Name": "Bà Rịa",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/baria.480p.stream/playlist.m3u8?st=TzdUIF0j4ZBJagLPIHbP9Q&e=1523923211",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/25/506616-ba-ria.png"
+    },
+    {
+      "Name": "Kiên Giang 1",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/kiengiang1.480p.stream/playlist.m3u8?st=OLUQJAC1mri1kaPNNSBRQQ&e=1523923211",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/poster/2015/05/08/487175-Kien-Giang-1-grey.png"
+    },
+    {
+      "Name": "Hậu Giang",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/haugiang.480p.stream/playlist.m3u8?st=kXavLfddoD0ftt6QjCDSzg&e=1523923211",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/10/306714-logo_haugiang.png"
+    },
+    {
+      "Name": "Khánh Hòa",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/khanhhoa.480p.stream/playlist.m3u8?st=Sb30QGpMqU0-t-JJTDUm-Q&e=1523923212",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/25/830309-khanh-hoa.png"
+    },
+    {
+      "Name": "Lạng Sơn",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/langson.480p.stream/playlist.m3u8?st=JRER33qq7biR3xtkqiupEQ&e=1523923213",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/25/614844-lang-son.png"
+    },
+    {
+      "Name": "Bình Định",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/binhdinh.480p.stream/playlist.m3u8?st=HLekY_TE-wFoYih-OCP7VQ&e=1523923213",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/25/799370-binh-dinh.png"
+    },
+    {
+      "Name": "Nam Định",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/namdinh.480p.stream/playlist.m3u8?st=xpjPIVxs8kxeRXwCX0RAUA&e=1523924582",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/25/857434-nam-dinh.png"
+    },
+    {
+      "Name": "Lào Cai",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/laocai.480p.stream/playlist.m3u8?st=nvBkfzTnIQVoLtHxTfGcqg&e=1523924582",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/24/327466-lao-cai.png"
+    },
+    {
+      "Name": "Quảng Nam",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/quangnam.480p.stream/playlist.m3u8?st=_J2Cr4F8EbYFEhE8UhQpwA&e=1523924582",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/24/101393-quang-nam.png"
+    },
+    {
+      "Name": "Sóc Trăng",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/soctrang.480p.stream/playlist.m3u8?st=M3T25Hac0rcrvCZC5E4CvA&e=1523924583",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/24/875961-soc-trang.png"
+    },
+    {
+      "Name": "Đắc Lắc",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/daclac.480p.stream/playlist.m3u8?st=KW7MzQMkK-iUrfNodFUhZA&e=1523924583",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/24/875961-dac-lac.png"
+    },
+    {
+      "Name": "Đắc Nông",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/dacnong.480p.stream/playlist.m3u8?st=j1f3ypVv70wnc-WUg8bTPg&e=1509231585",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/24/291019-dak-nong.png"
+    },
+    {
+      "Name": "Bắc Giang",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/bacgiang.480p.stream/playlist.m3u8?st=-wEzyjb_1EGNRyG8CgZbkA&e=1523923209",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/12/832542-bac-giang.png"
+    },
+    {
+      "Name": "Bạc Liêu",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/baclieu.480p.stream/playlist.m3u8?st=RfnBZlGn0nxv2vz2yhoJOw&e=1523923209",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/11/735010-bac-lieu.png"
+    },
+    {
+      "Name": "Hải Dương",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/haiduong.480p.stream/playlist.m3u8?st=6gl4qGsQksPbHiJiKZrPBQ&e=1523923210",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/11/269625-haiduong_logo.png"
+    },
+    {
+      "Name": "Ninh Bình",
+      "url": "http://htv-mb.live.cdn.fptplay.net/htvonline/ninhbinh.480p.stream/playlist.m3u8?st=sjwGbrqDlb17zKndxYjqPA&e=1523923210",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "http://img.hplus.com.vn/355x200/banner/2015/02/11/431037-NINHBINH-big.png"
+    },
+    {
+      "Name": "Cà Mau",
+      "url": "https://livecdn.fptplay.net/sdc/camau_hls.smil/playlist.m3u8?token=eyJzZXJ2ZXJfdGltZSI6IDE1MjMxMTAwNTEsICJyZXNvdXJjZV9pZCI6ICJjYW1hdV9obHMiLCAicGFydG5lcl9pZCI6ICI1NzEzYmU4YzA3ZTQyZWQ0YjkxNjM4OGUiLCAidG9rZW4iOiAiNjdjMWUwZDQ3MWExM2IzZDg3MmRmNjQ1ZWZiMmQzNjciLCAidmFsaWRfbWludXRlcyI6IDE1MjMxOTY0NTEsICJybWlwIjogIjE4My44MC4xODUuMTQ4In0=",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "https://i.imgur.com/3bOueGk.png"
+    },
+    {
+      "Name": "Nghệ An",
+      "url": "https://livecdn.fptplay.net/sda/nghean_hls.smil/playlist.m3u8?token=eyJzZXJ2ZXJfdGltZSI6IDE1MjMxMTAwNTIsICJyZXNvdXJjZV9pZCI6ICJuZ2hlYW5faGxzIiwgInBhcnRuZXJfaWQiOiAiNTcxM2JlOGMwN2U0MmVkNGI5MTYzODhlIiwgInRva2VuIjogIjY3NDliZDA2MjEyYWM3YTIwZjczYzg1NDNmMjMwMmRmIiwgInZhbGlkX21pbnV0ZXMiOiAxNTIzMTk2NDUyLCAicm1pcCI6ICIxODMuODAuMTg1LjE0OCJ9",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "https://static.fptplay.net/static/img/share/channels/icon_channel_nghe-an_145578842534.png?w=345&h=210&mode=scale"
+    },
+    {
+      "Name": "Tiền Giang",
+      "url": "https://livecdn.fptplay.net/sda/tiengiang_hls.smil/playlist.m3u8?token=eyJzZXJ2ZXJfdGltZSI6IDE1MjMxMDk0MzAsICJyZXNvdXJjZV9pZCI6ICJ0aWVuZ2lhbmdfaGxzIiwgInBhcnRuZXJfaWQiOiAiNTcxM2JlOGMwN2U0MmVkNGI5MTYzODhlIiwgInRva2VuIjogIjA3ODE4MGRjMTVhM2IyN2JhMzE2OWUwNWExMzk0ZTBiIiwgInZhbGlkX21pbnV0ZXMiOiAxNTIzMTk1ODMwLCAicm1pcCI6ICIxODMuODAuMTg1LjE0OCJ9",
+      "url2": "",
+      "url3": "",
+      "url4": "",
+      "url5": "",
+      "image": "https://static.fptplay.net/static/img/share/channels/icon_channel_tien-giang_145734128144.jpg?w=345&h=210&mode=scale"
+    }
+  ]
+}
